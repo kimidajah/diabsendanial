a4504454ced2f07789746907152b51dc65902f16
kimidajah <alucard142368@gmail.com>
2026-07-12 22:56:51 +0700

    memperbaiki responsivitas navigasi role wakasek di hp dengan menambahkan menu Laporan dan Pengaturan

--- resources/views/wakasek/layout.blade.php
+++ resources/views/wakasek/layout.blade.php
@@ -100,7 +100,7 @@
     </main>
 
     <!-- Mobile Navigation Bottom Bar -->
-    <div class="md:hidden bg-zinc-900 border-t border-zinc-800 sticky bottom-0 z-50 px-4 py-2 flex justify-around items-center">
+    <div class="md:hidden bg-zinc-900 border-t border-zinc-800 sticky bottom-0 z-50 px-2 py-2 flex justify-around items-center">
         <a href="{{ route('wakasek.dashboard') }}" 
             class="flex flex-col items-center justify-center py-1 {{ request()->routeIs('wakasek.dashboard') ? 'text-indigo-600' : 'text-zinc-400 hover:text-zinc-200' }}">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
@@ -121,6 +121,21 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.375M9 18h3.375m1.875-12h7.5c.621 0 1.125.504 1.125 1.125v17.25c0 .621-.504 1.125-1.125 1.125h-11.25a9 9 0 01-9-9v-9c0-.621.504-1.125 1.125-1.125h5.625M9 3.75h.008v.008H9V3.75z" />
             </svg>
             <span class="text-[9px] mt-1 font-medium">Persetujuan</span>
+        </a>
+        <a href="{{ route('wakasek.reports') }}" 
+            class="flex flex-col items-center justify-center py-1 {{ request()->routeIs('wakasek.reports*') ? 'text-indigo-600' : 'text-zinc-400 hover:text-zinc-200' }}">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
+            </svg>
+            <span class="text-[9px] mt-1 font-medium">Laporan</span>
+        </a>
+        <a href="{{ route('wakasek.settings') }}" 
+            class="flex flex-col items-center justify-center py-1 {{ request()->routeIs('wakasek.settings*') ? 'text-indigo-600' : 'text-zinc-400 hover:text-zinc-200' }}">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.43l-1.003.828c-.293.241-.438.613-.43.992a7.723 7.723 0 010 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.43l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.991l-1.004-.827a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.28z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 01-6 0z" />
+            </svg>
+            <span class="text-[9px] mt-1 font-medium">Pengaturan</span>
         </a>
     </div>
 
